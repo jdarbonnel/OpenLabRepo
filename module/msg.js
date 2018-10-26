d20b100c9b98974f8c494158d80ffc0871338f7e
jdarbonnel <32743651+jdarbonnel@users.noreply.github.com>
2018-10-26 11:40:32 +0200

    Add files via upload

--- module/msg.js
+++ module/msg.js
@@ -6,7 +6,7 @@ module.exports = {
   * Once you a familiar with the commands you can Type for example: **book a room at FRA tomorrow from 9:00 to 10:00**.\n\n 
   * You will need to know the building ID you want to book a room at. If you don't know it, [click here](http://www.leparisien.fr/issy-les-moulineaux-92130/issy-les-moulineaux-decouvrez-le-futur-siege-d-orange-05-09-2017-7237572.php)\n\n 
   * type: **reset** or or **restart** or **cancel** at any time to cancel a request.\n\n 
-  * type **support** to join a Spark space to ask questions about this bot\n\n * [click here](https://www.youtube.com/watch?v=YA7yG-LIYxU) to watch a quick demo video of roomRequest`,
+  * type **support** to join a Spark space to ask questions about this bot\n\n * [click here](https://www.youtube.com/watch?v=DFO_6j8VhHc) to watch a quick demo video of roomRequest`,
 
   cancelled: `You current request has been cancelled.`,
 
@@ -14,5 +14,5 @@ module.exports = {
 
   error: `Sorry, we ran into an issue booking the room!  This is usually caused by an unknown timezone for your region or your spark email address doesn't match a mailbox on this exchange system. Please type _support_ to join our spark room. Let us know what building you are trying to book a room at and we can fix it.`,
   
-  support: `You have been added to the Room Request Spark space.`
+  support: `You have been added to the Room Request Spark Space.`
 };
